52df52f0a00e2eed3ab10519f932fb9374a906e3
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-08-10 10:48:43 +1200

    Added support for powers of 1-9

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace CSharp_Test_Field
 {
@@ -15,7 +16,7 @@ namespace CSharp_Test_Field
     {
         public static double Calculate(string equation)
         {
-            int integer;
+            int integer; // Used to check if "equation[i]" equals a number
 
             // Arrays to store and get the values of numbers or operators
 
@@ -27,13 +28,9 @@ namespace CSharp_Test_Field
             int indexNumber = 0;
             int indexOperand = 0;
 
-            // Variable to store the result
+            double result = 0; // Variable to store the result
 
-            double result = 0;
-
-            // Reading and splitting up the equation
-
-            for (int i = 0; i < equation.Length; i++)
+            for (int i = 0; i < equation.Length; i++) // Reading and splitting up the equation
             {
 
                 // Getting the numbers. Will get algarism per algarism, but they will be put together in a string, then converted to an integer
@@ -45,13 +42,17 @@ namespace CSharp_Test_Field
 
                 // Getting the operators. Will be used later to identify which operation has to be done
 
-                else if (equation[i] == '-' || equation[i] == '+' || equation[i] == '*' || equation[i] == '/')
+                else if (equation[i] == '-' || equation[i] == '+' || equation[i] == '*' || equation[i] == '/' || equation[i] == '^')
                 {
                     operands[indexOperand++] = equation[i];
 
-                    //If the current item is an operator, the number array will start storing a new number
-
-                    indexNumber++;
+                    if (equation[i] == '^')
+                    {
+                        i++;
+                        operands[indexOperand++] = equation[i];
+                    }
+                    
+                    indexNumber++; //Start new number after operator
                 }
 
                 //If the current item is neither number or operator, it will be ignored and printed
@@ -66,6 +67,7 @@ namespace CSharp_Test_Field
 
             result = Convert.ToInt32(numbers[0]);
 
+            //Actual calculation now
             // 'i' for numbers, 'j' for operators
 
             for (int j = 0, i = 1; i < numbers.Length && j < operands.Length; i++, j++)
@@ -83,6 +85,9 @@ namespace CSharp_Test_Field
                         break;
                     case '/':
                         result /= Convert.ToInt32(numbers[i]);
+                        break;
+                    case '^':
+                        result = Math.Pow(result, Convert.ToInt32(operands[1 + j++].ToString()));
                         break;
                     default:
                         break;
